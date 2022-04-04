3616d59694927382790ff8e4179715d0ab2bcd2f
praveen <praveen991210@gmail.com>
2022-04-04 13:43:26 +0530

    int

--- src/First.java
+++ src/First.java
@@ -1,8 +1,6 @@
 
 public class First {
 public static void main(String[] args) {
-		System.out.println("Hello first");
-		System.out.println("Hello first");
-		System.out.println("Hello first");
+
 	}
 }
